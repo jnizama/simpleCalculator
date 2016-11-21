414903546f2fb4029417becba4c82e9fc6a205b1
jnizama <jorgenizamarios@gmail.com>
2016-11-21 19:47:16 -0200

    Initial Commit

---
+++ simpleCalculator/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  simpleCalculator
+//
+//  Created by Jorge on 21/11/16.
+//  Copyright © 2016 Jorge. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
